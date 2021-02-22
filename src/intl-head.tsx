d47545b0a4201cef38ae459293cce4619ba2e553
Helio <helios25@gmail.com>
2021-02-22 16:56:39 +0000

    :fire: removed language manipulation, this should be done outside

--- src/intl-head.tsx
+++ src/intl-head.tsx
@@ -13,32 +13,22 @@ interface IntlHeadProps {
 }
 
 export function IntlHead({ children, title, language }: IntlHeadProps): ReactElement {
-  const { locale: lang, query, asPath, locales, pathname, defaultLocale } = useRouter();
+  const { locale, query, asPath, locales, pathname } = useRouter();
 
-  type SupportedLocale = typeof locales[number];
-  const isSupportedLocale = (locale: SupportedLocale): boolean => locales.includes(locale);
-
-  const toSupportedLocale = (lang: string): SupportedLocale => {
-    if (lang?.length === 2) {
-      return locales.find((l) => {
-        const [languageCode] = l.split('-');
-        return languageCode === lang;
-      });
-    }
-    return isSupportedLocale(lang) ? lang : defaultLocale;
-  };
-
-  const locale = toSupportedLocale(language) ?? lang;
-
-  const canonicalUrl = useGetCanonicalUrl({ locale, asPath, query });
-  const alternateLinks = useGetAlternateLinks({ locales, locale, asPath, pathname });
+  const canonicalUrl = useGetCanonicalUrl({ locale: language ?? locale, asPath, query });
+  const alternateLinks = useGetAlternateLinks({
+    locales,
+    locale: language ?? locale,
+    asPath,
+    pathname,
+  });
   const origin = useGetOriginUrl();
 
   return (
     <Head>
       <title>{title}</title>
       {alternateLinks}
-      {pathname !== '/' && <link rel="canonical" href={`${origin}/${canonicalUrl}`} />}
+      {pathname !== '/' && <link rel="canonical" href={`${origin}${canonicalUrl}`} />}
       {children}
     </Head>
   );
