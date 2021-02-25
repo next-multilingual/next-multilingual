c1da7257b14add5b4b5ad52ce0c6ebb81272395f
Helio <helios25@gmail.com>
2021-02-25 21:22:40 +0000

    :fire: removed unnecessary file

--- example/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { AppProps } from 'next/app';
-import { ReactElement } from 'react';
-
-function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
