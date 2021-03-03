6216cb71ecc0b742eb413f235ad59d4e8b88f612
Helio <helios25@gmail.com>
2021-03-03 19:55:43 +0000

    :recycle: moved Language Switcher to it's own file

---
+++ example/components/LanguageSwitcher.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from 'react';
+import { IntlLink } from '../../lib/intl-link';
+
+import styles from './LanguageSwitcher.module.css';
+
+const LanguageSwitcher = ({ locales }: { locales: string[] }): ReactElement => (
+  <ul className={styles.languageSwitcherContainer}>
+    {locales.map((locale) => (
+      <li key={locale}>
+        <IntlLink href="/" locale={locale}>
+          {locale}
+        </IntlLink>
+      </li>
+    ))}
+  </ul>
+);
+
+export default LanguageSwitcher;
