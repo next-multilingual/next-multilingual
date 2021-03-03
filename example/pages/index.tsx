9cfec71113bea98b0f448de433a11d9278bab73f
Helio <helios25@gmail.com>
2021-03-03 19:58:37 +0000

    :sparkles: Wrapped pages with Layout component

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,40 +1,38 @@
-import { IntlLink } from '../../lib/intl-link';
+import accept from '@hapi/accept';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import { useRouter } from 'next/router';
+import { ReactElement } from 'react';
+import { IntlLink } from '../../lib/intl-link';
+import Layout from '../layout/Layout';
 
-export default function IndexPage() {
+export default function IndexPage({ language }): ReactElement {
   const router = useRouter();
   const { locale, locales, defaultLocale } = router;
 
   return (
-    <div>
+    <Layout title="Home Page" language={language}>
       <h1>Homepage</h1>
       <p>Current locale: {locale}</p>
       <p>Default locale: {defaultLocale}</p>
       <p>Configured locales: {JSON.stringify(locales)}</p>
-      <LanguageSwitcher locales={locales} locale={locale}></LanguageSwitcher>
-      <br />
       <br />
       <IntlLink href="/about-us" locale={locale}>
         <a>About us page</a>
       </IntlLink>
-    </div>
+    </Layout>
   );
 }
 
-type LanguageSwitcherProps = {
-  locale: string;
-  locales: string[];
-};
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  locales,
+  locale
+}: GetServerSidePropsContext) => {
+  const language = accept.language(req.headers['accept-language'], locales);
 
-function LanguageSwitcher(props: LanguageSwitcherProps) {
-  const { locale, locales } = props;
-  for (let supportedLocale of locales) {
-    if (locale !== supportedLocale) {
-      return (
-        <IntlLink href="/" locale={supportedLocale}>
-          <a>Switch language</a>
-        </IntlLink>
-      );
+  return {
+    props: {
+      language: locale || language
     }
-  }
-}
+  };
+};
