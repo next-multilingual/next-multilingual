d5d29b9c931d6dd3bed3f660a4600d8d52aea102
Helio <helios25@gmail.com>
2021-03-05 17:47:50 +0000

    :fire: removed locale from props

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -3,12 +3,12 @@ import { ReactElement } from 'react';
 import { IntlLink } from '../../lib/intl-link';
 import Layout from '../layout/Layout';
 
-export default function IndexPage({ language }): ReactElement {
+export default function IndexPage(): ReactElement {
   const router = useRouter();
   const { locale, locales, defaultLocale } = router;
 
   return (
-    <Layout title="Home Page" language={language}>
+    <Layout title="Home Page">
       <h1>Homepage</h1>
       <p>Current locale: {locale}</p>
       <p>Default locale: {defaultLocale}</p>
