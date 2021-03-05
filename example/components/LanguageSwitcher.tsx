e9f293336ec7de59c1222479775a490215fa26ef
Helio <helios25@gmail.com>
2021-03-05 17:12:57 +0000

    :recycle: removed 'catchAll' locale

--- example/components/LanguageSwitcher.tsx
+++ example/components/LanguageSwitcher.tsx
@@ -5,13 +5,15 @@ import styles from './LanguageSwitcher.module.css';
 
 const LanguageSwitcher = ({ locales }: { locales: string[] }): ReactElement => (
   <ul className={styles.languageSwitcherContainer}>
-    {locales.map((locale) => (
-      <li key={locale}>
-        <IntlLink href="/" locale={locale}>
-          {locale}
-        </IntlLink>
-      </li>
-    ))}
+    {locales
+      .filter((l) => l !== 'catchAll')
+      .map((locale) => (
+        <li key={locale}>
+          <IntlLink href="/" locale={locale}>
+            {locale}
+          </IntlLink>
+        </li>
+      ))}
   </ul>
 );
 
