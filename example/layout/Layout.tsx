2637b5984307c898a9a9f76b94d0001b79bf3d6f
Helio <helios25@gmail.com>
2021-03-07 08:59:11 +0000

    Revert ":recycle: moved useCanonicalUrl to example as it is not part of I18n"
    This reverts commit f82dd6b3fb0e5a4660a64ad772dc7a6571b269b5.

--- example/layout/Layout.tsx
+++ example/layout/Layout.tsx
@@ -2,28 +2,25 @@ import { useRouter } from 'next/router';
 import { ReactElement, ReactNode } from 'react';
 import { IntlHead } from '../../lib/intl-head';
 import LanguageSwitcher from '../components/LanguageSwitcher';
-import { useCanonicalUrl } from '../hooks/useCanonicalUrl';
 
 import styles from './Layout.module.css';
 
 interface LayoutProps {
   title: string;
+  language?: string;
   children: ReactNode;
 }
 
-const Layout = ({ title, children }: LayoutProps): ReactElement => {
-  const { locales, locale } = useRouter();
-  const canonicalUrl = useCanonicalUrl(locale);
-
+const Layout = ({ title, language, children }: LayoutProps): ReactElement => {
+  const { locales } = useRouter();
   return (
     <div className={styles.container}>
-      <IntlHead>
+      <IntlHead language={language}>
         <title>{title}</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
-        <link rel="canonical" href={canonicalUrl} />
       </IntlHead>
       <header className={styles.headerContainer}>
         <link rel="alternate" href="/" hrefLang="pt" />
--- src/intl-head.tsx
+++ src/intl-head.tsx
@@ -2,20 +2,24 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { ReactElement, ReactNode } from 'react';
 import { useAlternateLinks } from './hooks/useAlternateLinks';
+import { useCanonicalUrl } from './hooks/useCanonicalUrl';
 
 interface IntlHeadProps {
   children?: ReactNode;
+  currentLocale?: string;
 }
 
-export function IntlHead({ children }: IntlHeadProps): ReactElement {
+export function IntlHead({ children, currentLocale }: IntlHeadProps): ReactElement {
   const { locale } = useRouter();
-  const alternateLinks = useAlternateLinks(locale);
+  const canonicalUrl = useCanonicalUrl(currentLocale ?? locale);
+  const alternateLinks = useAlternateLinks(currentLocale ?? locale);
 
   return (
     <Head>
       {alternateLinks.map(({ href, hrefLang }) => (
         <link rel="alternate" href={`${href}`} hrefLang={hrefLang} key={hrefLang} />
       ))}
+      <link rel="canonical" href={canonicalUrl} />
       {children}
     </Head>
   );
