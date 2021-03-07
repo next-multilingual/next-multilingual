7b8354b87c8e78c3d312608d0491f6ddf4ff8aa9
Helio <helios25@gmail.com>
2021-03-07 08:57:44 +0000

    Revert ":fire: removed locale from props"
    This reverts commit d5d29b9c931d6dd3bed3f660a4600d8d52aea102.

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -3,12 +3,12 @@ import { ReactElement } from 'react';
 import { IntlLink } from '../../lib/intl-link';
 import Layout from '../layout/Layout';
 
-export default function IndexPage(): ReactElement {
+export default function IndexPage({ language }): ReactElement {
   const router = useRouter();
   const { locale, locales, defaultLocale } = router;
 
   return (
-    <Layout title="Home Page">
+    <Layout title="Home Page" language={language}>
       <h1>Homepage</h1>
       <p>Current locale: {locale}</p>
       <p>Default locale: {defaultLocale}</p>
