4e7b4b3e889a940e4a29a58258d6462680ae996e
Helio <helios25@gmail.com>
2021-03-12 16:46:27 +0000

    :recycle: make sure we add x-default alternate link with proper path in all routes

--- src/helpers/getSourceUrl.ts
+++ src/helpers/getSourceUrl.ts
@@ -5,8 +5,6 @@ interface GetSourceUrlProps {
 }
 export const getSourceUrl = ({ rewrites, locale, path }: GetSourceUrlProps): string => {
   const lcPath = `/${locale}${path}`;
-  const match = rewrites.find(({ destination, locale }) => {
-    return locale === false && destination === lcPath;
-  });
-  return match ? match.source : path;
+  const match = rewrites.find(({ destination, locale }) => locale === false && destination === lcPath);
+  return match ? match.source : lcPath;
 };
--- src/hooks/useAlternateLinks.ts
+++ src/hooks/useAlternateLinks.ts
@@ -4,34 +4,37 @@ import { getBasePath } from '../helpers/getBasePath';
 import { getOrigin } from '../helpers/getOrigin';
 import { getSourceUrl } from '../helpers/getSourceUrl';
 import { useRewrites } from './useRewrites';
+import { removeFirstSlash } from '../helpers/paths';
 
 interface AlternateLink {
   href: string;
   hrefLang: string;
 }
 
-export const useAlternateLinks = (currentLocale: string): AlternateLink[] => {
-  const { basePath, pathname, locales } = useRouter();
+export const useAlternateLinks = (locale: string): AlternateLink[] => {
+  const { basePath, pathname, locales, defaultLocale } = useRouter();
   const rewrites = useRewrites();
   const [alternateLinks, setAlternateLinks] = useState<AlternateLink[]>([]);
 
   useEffect(() => {
     const origin = getOrigin();
     const alternateLinks = locales
-      .filter((l) => l !== 'catchAll')
       .map((lang) => {
         const alternateLink = getSourceUrl({ rewrites, locale: lang, path: pathname });
         return {
-          href: `${origin}${getBasePath(basePath)}${lang}${alternateLink}`,
+          href: `${origin}${getBasePath(basePath)}${removeFirstSlash(alternateLink)}`,
           hrefLang: lang,
         };
       });
-    setAlternateLinks(
-      pathname !== '/'
-        ? alternateLinks
-        : [...alternateLinks, { href: `${origin}${getBasePath(basePath)}`, hrefLang: 'x-default' }]
+
+    const withPathname = pathname !== '/'
+      ? removeFirstSlash(pathname)
+      : ''
+
+    const _href = `${origin}${getBasePath(basePath)}${withPathname}`
+    setAlternateLinks([...alternateLinks, { href: _href, hrefLang: 'x-default' }]
     );
-  }, [basePath, currentLocale, locales, pathname, rewrites]);
+  }, [basePath, locales, pathname, rewrites]);
 
   return alternateLinks;
 };
