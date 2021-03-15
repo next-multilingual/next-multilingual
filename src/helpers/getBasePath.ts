60d894f203f871527776c3c70986aa78e5824bf8
Helio <helios25@gmail.com>
2021-03-15 22:14:07 +0000

    :bulb: added jsdoc

--- src/helpers/getBasePath.ts
+++ src/helpers/getBasePath.ts
@@ -1,2 +1,8 @@
-export const getBasePath = (basePath?: string): string =>
-  basePath ? basePath.replace('/', '') + '/' : '';
+/**
+ * If we have a basePath we remove the initial slash and add it at the end and return it
+ * @param basePath { string? } - the basePath
+ */
+export function getBasePath(basePath?: string): string {
+  console.warn('basePath in getBasePath', basePath);
+  return basePath ? basePath.replace('/', '') + '/' : '';
+}
