49bf9e41d1501994b5e4e3814433ff6c5f3babcf
nbouvrette <bouvrette.nicolas@gmail.com>
2021-04-01 17:49:09 -0400

    First string implementation in about-us

--- example/pages/about-us.tsx
+++ example/pages/about-us.tsx
@@ -1,13 +1,19 @@
+import { useRouter } from 'next/router';
 import Layout from '../layout/Layout';
-import messages from './about-us.fr-CA.properties';
+import Messages from 'messageformat-runtime/messages';
+import frCA from './about-us.fr-CA.properties';
+import enCA from './about-us.en-CA.properties';
 
-console.warn('messages', messages);
+const messages = new Messages({ 'fr-CA': frCA, 'en-CA': enCA }, 'en-CA');
 
 export default function AboutUs() {
+  const router = useRouter();
+  const { locale, locales, defaultLocale } = router;
+
   return (
     <Layout title="About us">
-      <h1>This is the about us</h1>
-      <p>We are the best.</p>
+      <h1>{messages.get('title', {}, locale)}</h1>
+      <p>{messages.get('details', {}, locale)}</p>
     </Layout>
   );
 }
