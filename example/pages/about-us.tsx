82ba7f2df5fb37a21bb39540ceda14178608ca69
nbouvrette <bouvrette.nicolas@gmail.com>
2021-04-01 18:03:37 -0400

    Simplify string getter

--- example/pages/about-us.tsx
+++ example/pages/about-us.tsx
@@ -4,16 +4,20 @@ import Messages from 'messageformat-runtime/messages';
 import frCA from './about-us.fr-CA.properties';
 import enCA from './about-us.en-CA.properties';
 
-const messages = new Messages({ 'fr-CA': frCA, 'en-CA': enCA }, 'en-CA');
-
 export default function AboutUs() {
   const router = useRouter();
-  const { locale, locales, defaultLocale } = router;
+  const { locale, defaultLocale } = router;
+  const messages = new Messages(
+    { 'fr-CA': frCA, 'en-CA': enCA },
+    defaultLocale
+  );
+
+  messages.locale = locale;
 
   return (
     <Layout title="About us">
-      <h1>{messages.get('title', {}, locale)}</h1>
-      <p>{messages.get('details', {}, locale)}</p>
+      <h1>{messages.get('title')}</h1>
+      <p>{messages.get('details')}</p>
     </Layout>
   );
 }
