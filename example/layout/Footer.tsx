850668da59758f84c1743eef65533249d01035fd
nbouvrette <bouvrette.nicolas@gmail.com>
2021-04-05 19:35:26 -0400

    Fixing bad type for messages

--- example/layout/Footer.tsx
+++ example/layout/Footer.tsx
@@ -2,10 +2,14 @@ import { useRouter } from 'next/router';
 import { ReactElement, useState } from 'react';
 import styles from './Footer.module.css';
 
+type Messages = {
+  [key: string]: string;
+};
+
 const Footer = (): ReactElement => {
   const { locale } = useRouter();
-  const [messages, setMessages] = useState<{ Record: string } | undefined>();
-  const messagesLoader = (): Promise<{ Record: string }> =>
+  const [messages, setMessages] = useState<Messages | undefined>();
+  const messagesLoader = (): Promise<Messages> =>
     import(`./footer.${locale}.properties`).then((res) => res.default);
   messagesLoader().then((messages) => setMessages(messages));
   return messages ? (
