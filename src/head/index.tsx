36b42c40714785f288c36d69e7655cdcbfe3b24b
nbouvrette <bouvrette.nicolas@gmail.com>
2021-06-19 16:23:01 -0400

    💡 Add Jsdoc

--- src/head/index.tsx
+++ src/head/index.tsx
@@ -2,6 +2,14 @@ import Head from 'next/head';
 import React from 'react';
 import { useAlternateLinks } from '../hooks/use-alternate-links';
 
+/**
+ * MulHead is a wrapper around Next.js' `Head` that provides alternate links with localized URLs.
+ *
+ * This component should have a positive impact on SEO by both providing alternative link markup following the guidelines
+ * provided by Google and also optimizing keyword relevancy by using localized URLs.
+ *
+ * @returns The Next.js `Head` component, including alternative links for SEO.
+ */
 export function MulHead({ children }: { children: React.ReactNode }): JSX.Element {
   const alternateLinks = useAlternateLinks();
 
