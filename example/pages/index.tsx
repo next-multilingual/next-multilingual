3500ab09fed35b0b67a06194b6bc0a19d16dc563
nbouvrette <bouvrette.nicolas@gmail.com>
2021-06-20 14:32:30 -0400

    🎨Add detailed i18n information on homepage

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -2,7 +2,8 @@ import Cookies from 'nookies';
 import {
   getActualLocales,
   getActualDefaultLocale,
-  normalizeLocale
+  normalizeLocale,
+  getActualLocale
 } from 'next-multilingual';
 import type { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import { useRouter } from 'next/router';
@@ -13,22 +14,58 @@ import type {
   MulMessages,
   MulMessagesServerSideProps
 } from 'next-multilingual/messages';
+import styles from './index.module.css';
 
 export default function IndexPage({
   messages,
-  currentLocale
+  actualLocale
 }: MulMessagesServerSideProps): ReactElement {
   const router = useRouter();
-  router.locale = currentLocale; // Overwrite locale with the resolved locale.
   const { locales, defaultLocale, locale } = router;
+  router.locale = actualLocale; // Overwrite the router's locale with the actual locale for components to work.
 
   return (
     <Layout title={messages.title}>
-      <h1>{messages.headline}</h1>
-      <p>Router locale: {locale}</p>
-      <p>Default locale: {defaultLocale}</p>
-      <p>Configured locales: {JSON.stringify(locales)}</p>
-      <br />
+      <h1 className={styles.headline}>{messages.headline}</h1>
+      <div>
+        <h2 className={styles.subHeader}>{messages.subHeader}</h2>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>{messages.columnInformation}</th>
+              <th>{messages.columnNextJs}</th>
+              <th>{messages.columnActual}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{messages.rowLocale}</td>
+              <td>{normalizeLocale(locale)}</td>
+              <td>{normalizeLocale(actualLocale)}</td>
+            </tr>
+            <tr>
+              <td>{messages.rowDefaultLocale}</td>
+              <td>{normalizeLocale(defaultLocale)}</td>
+              <td>
+                {normalizeLocale(
+                  getActualDefaultLocale(locales, defaultLocale)
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>{messages.rowConfiguredLocales}</td>
+              <td>
+                {locales.map((locale) => normalizeLocale(locale)).join(', ')}
+              </td>
+              <td>
+                {getActualLocales(locales, defaultLocale)
+                  .map((locale) => normalizeLocale(locale))
+                  .join(', ')}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </Layout>
   );
 }
@@ -36,19 +73,15 @@ export default function IndexPage({
 export const getServerSideProps: GetServerSideProps = async (
   getServerSidePropsContext: GetServerSidePropsContext
 ) => {
-  const {
-    req,
-    locale,
-    locales,
-    defaultLocale: multilingual
-  } = getServerSidePropsContext;
+  const { req, locale, locales, defaultLocale } = getServerSidePropsContext;
 
-  const actualLocales = getActualLocales(locales, multilingual);
-  const actualDefaultLocale = getActualDefaultLocale(locales, multilingual);
+  const actualLocales = getActualLocales(locales, defaultLocale);
+  const actualDefaultLocale = getActualDefaultLocale(locales, defaultLocale);
   const cookies = Cookies.get(getServerSidePropsContext);
-  let currentLocale = locale;
+  let actualLocale = getActualLocale(locale, defaultLocale, locales);
 
-  if (locale === multilingual) {
+  // When Next.js tries to use the default locale, try to find a better one.
+  if (locale === defaultLocale) {
     // TODO: add a script that sets NEXT_LOCALE on initial page load (client-side)
     let cookieLocale = cookies['NEXT_LOCALE'];
     if (cookieLocale && !actualLocales.includes(cookieLocale)) {
@@ -57,7 +90,7 @@ export const getServerSideProps: GetServerSideProps = async (
       cookieLocale = undefined;
     }
 
-    currentLocale = cookieLocale
+    actualLocale = cookieLocale
       ? cookieLocale
       : resolveAcceptLanguage(
           req.headers['accept-language'],
@@ -67,13 +100,13 @@ export const getServerSideProps: GetServerSideProps = async (
   }
 
   const messages = (
-    await import(`./index.${normalizeLocale(currentLocale)}.properties`)
+    await import(`./index.${normalizeLocale(actualLocale)}.properties`)
   ).default as MulMessages;
 
   return {
     props: {
       messages,
-      currentLocale
+      actualLocale
     }
   };
 };
