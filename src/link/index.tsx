7f4b19a664b47e53dc289b1610baa909980b9e0e
Eemeli Aro <eemeli@gmail.com>
2021-06-22 17:54:48 +0200

    feat: Suppress hydration warnings for <MulLink href>

--- src/link/index.tsx
+++ src/link/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { Children, cloneElement, ReactElement } from 'react';
 import Link, { LinkProps } from 'next/link';
 import { useRouter } from 'next/router';
 import { useLocalizedUrl } from '../hooks/use-localized-url';
@@ -13,6 +13,7 @@ import { useLocalizedUrl } from '../hooks/use-localized-url';
  * @returns The Next.js `Link` component with the correct localized URLs.
  */
 export function MulLink({
+  children,
   href,
   locale,
   ...props
@@ -20,5 +21,22 @@ export function MulLink({
   const router = useRouter();
   locale = locale ? locale : router.locale;
   const localizedUrl = useLocalizedUrl(locale, href);
-  return <Link href={localizedUrl} locale={locale} {...props} />;
+  try {
+    if (Children.only(children) && typeof children === 'object' && 'type' in children) {
+      // On the client, on first render useLocalizedUrl doesn't have access
+      // to the rewrites data, and therefore uses the non-localized URL.
+      // We suppress React's warning about the difference with the data
+      // provided by the server, as a proper resolution to this would require
+      // either including the rewrite data twice, or deeply refactoring how
+      // Next.js works.
+      children = cloneElement(children, { suppressHydrationWarning: true });
+    }
+  } catch (e) {
+    // ignore any error; handled by <Link>
+  }
+  return (
+    <Link href={localizedUrl} locale={locale} {...props}>
+      {children}
+    </Link>
+  );
 }
