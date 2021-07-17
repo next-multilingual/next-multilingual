56c354d52765166766b4be7d8cab28caff5da68b
nbouvrette <bouvrette.nicolas@gmail.com>
2021-07-17 13:42:40 -0400

    💄 Change wording and style on the `about-us` page

--- example/pages/about-us.tsx
+++ example/pages/about-us.tsx
@@ -1,12 +1,13 @@
 import { useMessages } from 'next-multilingual/messages';
 import type { ReactElement } from 'react';
 import Layout from '@/layout';
+import styles from './index.module.css';
 
 export default function AboutUs(): ReactElement {
   const messages = useMessages();
   return (
     <Layout title={messages.title}>
-      <h1>{messages.title}</h1>
+      <h1 className={styles.headline}>{messages.title}</h1>
       <p>{messages.details}</p>
     </Layout>
   );
