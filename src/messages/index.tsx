7915c8b8f6ded415e45308c11e72f1f85f13a63c
nbouvrette <bouvrette.nicolas@gmail.com>
2021-07-19 21:38:32 -0400

    📚 update jsdoc

--- src/messages/index.tsx
+++ src/messages/index.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 
 /**
- * Multilingual messages consist consist of unique keys and their localized strings.
+ * Multilingual messages consist of unique keys and their localized strings.
  */
 export type MulMessages = {
   readonly [key: string]: string;
@@ -14,6 +14,9 @@ export type MulMessagesCollection = {
   [key: string]: MulMessages;
 };
 
+/**
+ * Hook to get the localized messages specific to a Next.js component or page.
+ */
 export function useMessages(): MulMessages;
 export function useMessages(mulMessagesCollection?: MulMessagesCollection): MulMessages {
   const { locale } = useRouter();
