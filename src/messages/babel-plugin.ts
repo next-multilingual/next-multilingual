773e0ca1bdb22ea49a77f01bf24491a10fad490e
nbouvrette <bouvrette.nicolas@gmail.com>
2021-07-31 19:25:25 -0400

    🔨 refactor Babel plugin

--- src/messages/babel-plugin.ts
+++ src/messages/babel-plugin.ts
@@ -3,25 +3,25 @@ import { parse, resolve } from 'path';
 import type { MulMessagesCollection } from '.';
 import { parsePropertiesFile } from './properties';
 
-import * as BabelCoreNamespace from '@babel/core';
-import * as BabelTypesNamespace from '@babel/types';
-import { cloneNode } from '@babel/types';
+import * as BabelTypes from '@babel/types';
 import type { PluginObj, PluginPass, NodePath } from '@babel/core';
-import { parse as babelParse } from '@babel/parser';
+import template from '@babel/template';
 
-export type Babel = typeof BabelCoreNamespace;
-export type BabelTypes = typeof BabelTypesNamespace;
-export type ImportDeclaration = BabelTypesNamespace.ImportDeclaration;
-export type ImportSpecifier = BabelTypesNamespace.ImportSpecifier;
-export type ImportNamespaceSpecifier = BabelTypesNamespace.ImportNamespaceSpecifier;
-export type Identifier = BabelTypesNamespace.Identifier;
-const isImportNamespaceSpecifier = BabelTypesNamespace.isImportNamespaceSpecifier;
-const isImportSpecifier = BabelTypesNamespace.isImportSpecifier;
-const importDeclaration = BabelTypesNamespace.importDeclaration;
+export type Program = BabelTypes.Program;
+export type Statement = BabelTypes.Statement;
+export type ImportDeclaration = BabelTypes.ImportDeclaration;
+export type ImportSpecifier = BabelTypes.ImportSpecifier;
+export type ImportNamespaceSpecifier = BabelTypes.ImportNamespaceSpecifier;
+export type ImportDefaultSpecifier = BabelTypes.ImportDefaultSpecifier;
 
-// TODO: re-inject messages before imports (top of program)
-// TODO: re-inject variables right after first import
-// TODO: check if we can support dynamic `import()`
+export type Identifier = BabelTypes.Identifier;
+const isImportNamespaceSpecifier = BabelTypes.isImportNamespaceSpecifier;
+const isImportSpecifier = BabelTypes.isImportSpecifier;
+
+/** Target module to "babelify". */
+const TARGET_MODULE = 'next-multilingual/messages';
+/** Target function (of the target module) to "babelify". */
+const TARGET_FUNCTION = 'useMessages';
 
 /**
  * Class used to inject localized messages using Babel (a.k.a "babelified" messages).
@@ -61,124 +61,225 @@ function getBabelifiedMessages(sourceFilePath: string): string {
   return JSON.stringify(babelifiedMessages);
 }
 
-// Name of the target module to "babelify".
-const MODULE_NAME = 'next-multilingual/messages';
-// Import name of the target module to "babelify".
-const MODULE_IMPORT_NAME = 'useMessages';
-// Name of the injected variable that will contain the messages.
-const INJECTED_VARIABLE_NAME = 'messages';
+/**
+ * Verify if an import declaration node matches the target module.
+ *
+ * @param nodePath A node path object.
+ *
+ * @returns True is the node matches, otherwise false.
+ */
+function isMatchingModule(nodePath: NodePath): boolean {
+  if (!nodePath.isImportDeclaration()) return false;
+  if (nodePath.node.source.value !== TARGET_MODULE) return false;
+  return true;
+}
 
 /**
- * Babel plugin to inject multilingual messages into Next.js pages or components.
+ * Verify if a specifier matches the target function.
  *
- * @returns A plugin object to be used by Babel.
+ * @param nodePath A node path object.
+ *
+ * @returns True is the specifier matches, otherwise false.
+ */
+function isMatchingModuleImportName(
+  specifier: ImportDefaultSpecifier | ImportNamespaceSpecifier | ImportSpecifier
+): boolean {
+  return (
+    isImportSpecifier(specifier) && (specifier.imported as Identifier).name === TARGET_FUNCTION
+  );
+}
+
+/**
+ * Verify if an import declaration node matches the target module and function.
+ *
+ * @param nodePath A node path object.
+ *
+ * @returns True is the node matches, otherwise false.
+ */
+function isMatchingNamedImport(nodePath: NodePath): boolean {
+  return (
+    isMatchingModule(nodePath) &&
+    (nodePath.node as ImportDeclaration).specifiers.some((specifier) =>
+      isMatchingModuleImportName(specifier)
+    )
+  );
+}
+
+/**
+ * Verify if a namespace import declaration node matches the target module and function.
+ *
+ * @param nodePath A node path object.
+ *
+ * @returns True is the node matches, otherwise false.
+ */
+function isMatchingNamespaceImport(nodePath: NodePath): boolean {
+  return (
+    isMatchingModule(nodePath) &&
+    isImportNamespaceSpecifier((nodePath.node as ImportDeclaration).specifiers[0])
+  );
+}
+/**
+ * Class used to inject babelified messages.
+ */
+class Messages {
+  /** The program node path associated with the class. */
+  private programNodePath: NodePath<Program>;
+  /** The source file path associated with the class. */
+  private sourceFilePath: string;
+  /** The number of time the `getVariableName` was called. */
+  private getVariableNameCount = 0;
+  /** The unique variable name used relative to the program node path. */
+  private variableName: string;
+
+  /**
+   * Constructor.
+   *
+   * @param programNodePath The program node path associated with the class.
+   * @param pluginPass The `PluginPass` object associated with the class.
+   */
+  constructor(programNodePath: NodePath<Program>, pluginPass: PluginPass) {
+    this.programNodePath = programNodePath;
+    this.sourceFilePath = pluginPass.file.opts.filename;
+    this.variableName = this.programNodePath.scope.generateUidIdentifier('messages').name;
+  }
+
+  /**
+   * Get the unique variable name used relative to the program node path.
+   */
+  public getVariableName(): string {
+    this.getVariableNameCount++;
+    return this.variableName;
+  }
+
+  /**
+   * Inject the babelified messages to the program node path, if the variables name was used.
+   */
+  public injectIfMatchesFound(): void {
+    if (!this.getVariableNameCount) return;
+
+    // Inject the messages at the beginning o the file.
+    this.programNodePath.node.body.unshift(
+      template.ast(
+        `const ${this.variableName} = ${getBabelifiedMessages(this.sourceFilePath)};`
+      ) as Statement
+    );
+  }
+}
+
+/**
+ * Get a variable name to hijack either a named import or a namespace import.
+ *
+ * @param nodePath The node path from which to get the unique variable name.
+ * @param suffix The suffix of the variable name.
+ *
+ * @returns A unique variable name in the node path's scope.
+ */
+function getVariableName(nodePath: NodePath, suffix: string): string {
+  return nodePath.scope.generateUidIdentifier(`${TARGET_FUNCTION}${suffix}`).name;
+}
+
+/**
+ * "Hijack" a namespace (`import * as messages from`) import.
+ *
+ * This will simply copy the namespace on another function (because namespaces are readonly), and then bind the
+ * target function with the babelified messages. All bindings of the original namespace will be replaced by the
+ * hijacked namespace.
+ *
+ * @param nodePath The node path being hijacked.
+ * @param messages The object used to conditionally inject babelified messages.
+ */
+function hijackNamespaceImport(nodePath: NodePath<ImportDeclaration>, messages: Messages): void {
+  const node = nodePath.node;
+  const specifier = node.specifiers[0];
+  const currentName = specifier.local.name;
+
+  // This is the scope-unique variable name that will replace all matching namespace bindings.
+  const hijackedNamespace = getVariableName(nodePath, 'Namespace');
+
+  // Rename all bindings with the the new name (this excludes the import declaration).
+  const binding = nodePath.scope.getBinding(currentName);
+
+  binding.referencePaths.forEach((referencePath) => {
+    referencePath.scope.rename(currentName, hijackedNamespace, referencePath.parent);
+  });
+
+  // Insert the new "hijacked" namespace variable, with the correct binding.
+  nodePath.insertAfter(
+    template.ast(
+      `const ${hijackedNamespace} = ${currentName};` +
+        `${hijackedNamespace}.${TARGET_FUNCTION}.bind(${messages.getVariableName()});`
+    ) as Statement
+  );
+}
+
+/**
+ * "Hijack" a named (`import { useMessages } from`) import.
+ *
+ * This will simply bind the named import to the babelified messages, on a new function name. All bindings
+ * of the original function will replaced by the hijacked function.
+ *
+ * @param nodePath The node path being hijacked.
+ * @param messages The object used to conditionally inject babelified messages.
+ */
+function hijackNamedImport(nodePath: NodePath<ImportDeclaration>, messages: Messages): void {
+  const node = nodePath.node;
+
+  node.specifiers.forEach((specifier) => {
+    if (isMatchingModuleImportName(specifier)) {
+      // This is the scope-unique variable name that will replace all matching function bindings.
+      const hijackedFunction = getVariableName(nodePath, 'Function');
+
+      const currentName = specifier.local.name;
+
+      // Rename all bindings with the the new name (this excludes the import declaration).
+      const binding = nodePath.scope.getBinding(currentName);
+
+      binding.referencePaths.forEach((referencePath) => {
+        referencePath.scope.rename(currentName, hijackedFunction, referencePath.parent);
+      });
+
+      // Insert the new "hijacked" namespace variable, with the correct binding.
+      nodePath.insertAfter(
+        template.ast(
+          `const ${hijackedFunction} = ${currentName}.bind(${messages.getVariableName()});`
+        ) as Statement
+      );
+    }
+  });
+}
+
+/**
+ * This is the Babel plugin.
+ *
+ * This plugin will visit all files used by Next.js during the build time and inject the localized messages
+ * to the `useMessages` hook.
+ *
+ * What is supported:
+ *
+ * - Named imports (`import { useMessages } from`): this is how `useMessages` is meant to be used.
+ * - Namespace imports (`import * as messages from`): there is no reason for this but it's supported.
+ *
+ * What is not supported:
+ *
+ * - Dynamic `import()` statements.
+ *
+ * @returns A Babel plugin object.
  */
 export default function plugin(): PluginObj {
   return {
     visitor: {
-      Program(nodePath: NodePath, pluginPass: PluginPass) {
-        // Name of the variables referencing matching namespace imports (e.g. "import * as messages").
-        const namespaceImports: string[] = [];
-        // Name of the variables referencing matching named imports (e.g. "import { useMessage }").
-        const namedImports: string[] = [];
+      Program(programNodePath: NodePath<Program>, pluginPass: PluginPass) {
+        const messages = new Messages(programNodePath, pluginPass);
 
-        // Get all import declarations.
-        const importDeclarationNodePaths = (nodePath.get('body') as NodePath[]).filter((node) =>
-          node.isImportDeclaration()
-        ) as NodePath<ImportDeclaration>[];
-
-        // Make a copy of the node so we can restore it later after we rename all matching variables.
-        const originalImportDeclarationNodes = importDeclarationNodePaths.map((nodePath) => {
-          return cloneNode(nodePath.node, true);
-        }) as ImportDeclaration[];
-
-        // Check for all matching namespace an name imports and track them.
-        importDeclarationNodePaths.forEach((importDeclarationNodePath) => {
-          const importDeclarationNode = importDeclarationNodePath.node;
-          const specifiers = importDeclarationNode.specifiers as ImportSpecifier[];
-          const importedModuleName = importDeclarationNode.source.value;
-          if (importedModuleName !== MODULE_NAME) return;
-
-          specifiers.forEach((specifier) => {
-            if (isImportNamespaceSpecifier(specifier)) {
-              // Add the alias (variable name) of all matching namespace imports.
-              namespaceImports.push((specifier as ImportNamespaceSpecifier).local.name);
-            } else if (isImportSpecifier(specifier)) {
-              const importName = (specifier.imported as Identifier).name;
-              const importNameAlias = specifier.local.name;
-              if (importName === MODULE_IMPORT_NAME) {
-                // Add the alias (variable name) of all matching named imports.
-                namedImports.push(importNameAlias);
-              }
-            }
-          });
+        (programNodePath.get('body') as NodePath[]).forEach((bodyNodePath) => {
+          if (isMatchingNamespaceImport(bodyNodePath)) {
+            hijackNamespaceImport(bodyNodePath as NodePath<ImportDeclaration>, messages);
+          } else if (isMatchingNamedImport(bodyNodePath)) {
+            hijackNamedImport(bodyNodePath as NodePath<ImportDeclaration>, messages);
+          }
         });
 
-        // Stop if no matching imports.
-        if (!namespaceImports.length && !namedImports.length) {
-          return;
-        }
-
-        // This is the scope-unique variable name that will contain all messages.
-        const messagesVariable = nodePath.scope.generateUidIdentifier(INJECTED_VARIABLE_NAME);
-
-        // Create the code to inject, starting with the localized messages.
-        let codeToInject = `const ${messagesVariable.name} = ${getBabelifiedMessages(
-          pluginPass.file.opts.filename
-        )};`;
-
-        // If there are matching namespace imports, hijack the namespace variable.
-        if (namespaceImports.length) {
-          // This is the scope-unique variable name that will replace all matching namespace bindings.
-          const hijackedNamespace = nodePath.scope.generateUidIdentifier(
-            `${MODULE_IMPORT_NAME}Namespace`
-          );
-
-          // Copy the namespace to another variable first since its readonly.
-          codeToInject += `const ${hijackedNamespace.name} = ${namespaceImports[0]};`;
-          // Overwrite the function with the correct binding.
-          codeToInject += `${hijackedNamespace.name}.${MODULE_IMPORT_NAME} = ${namespaceImports[0]}.${MODULE_IMPORT_NAME}.bind(${messagesVariable.name});`;
-
-          // Rename all matching namespace imports variables reference in the global scope with the new hijacked namespace.
-          namespaceImports.forEach((namespaceImport) => {
-            // This will also rename the import declarations, so we will have to restore their original value.
-            nodePath.scope.rename(namespaceImport, hijackedNamespace.name);
-          });
-        }
-
-        // If there are matching named imports, hijack the named variable.
-        if (namedImports.length) {
-          // This is the scope-unique variable name that will replace all matching function bindings.
-          const hijackedFunction = nodePath.scope.generateUidIdentifier(
-            `${MODULE_IMPORT_NAME}Function`
-          );
-
-          // Hijack named imports with a copy of the function with the correct binding.
-          codeToInject += `const ${hijackedFunction.name} = ${namedImports[0]}.bind(${messagesVariable.name});`;
-
-          // Rename all matching named imports variables reference in the global scope with the new hijacked function.
-          namedImports.forEach((namedImport) => {
-            // This will also rename the import declarations, so we will have to restore their original value.
-            nodePath.scope.rename(namedImport, hijackedFunction.name);
-          });
-        }
-
-        // Because of the global scope rename, we need to set back original import declarations.
-        for (const importPosition in importDeclarationNodePaths) {
-          const hijackedImportDeclarationNodePath = importDeclarationNodePaths[importPosition];
-          const originalImportDeclarationNode = originalImportDeclarationNodes[importPosition];
-
-          // Replace the specifiers of the hijacked import declaration with its original value.
-          hijackedImportDeclarationNodePath.replaceWith(
-            importDeclaration(
-              originalImportDeclarationNode.specifiers,
-              hijackedImportDeclarationNodePath.node.source
-            )
-          );
-        }
-
-        // Inject the code directly after the last import.
-        importDeclarationNodePaths.pop().insertAfter(babelParse(codeToInject));
+        messages.injectIfMatchesFound();
       },
     },
   };
