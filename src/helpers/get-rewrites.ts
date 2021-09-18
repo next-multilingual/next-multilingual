cf3d68d1b0dec78bcea58b4c615c9cd80c015557
nbouvrette <bouvrette.nicolas@gmail.com>
2021-09-18 08:44:39 -0400

    ✨ add localized SSR URL support for Vercel

--- src/helpers/get-rewrites.ts
+++ src/helpers/get-rewrites.ts
@@ -1,10 +1,15 @@
 import { resolve } from 'path';
-import { readFileSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 import type { Rewrite } from 'next/dist/lib/load-custom-routes';
-import type { ManifestRewrites } from '../types';
+import { ManifestRewrites } from '../types';
 
 /** Local rewrite cache to avoid non-required file system operations. */
 let rewritesCache: Rewrite[];
+
+/** Object representing a simplified version of the route manifest files. */
+export type RoutesManifest = {
+  rewrites: Rewrite[];
+};
 
 /**
  * Get the Next.js `Rewrite` objects directly from the build manifest.
@@ -16,7 +21,25 @@ let rewritesCache: Rewrite[];
 export function getRewrites(): Rewrite[] {
   if (rewritesCache) return rewritesCache;
 
-  // Get the content from the Next.js build-manifest (.next/build-manifest.json).
+  // Try to get the content of the routes-manifest (.next/routes-manifest.json) first - this is only available on builds.
+  const routesManifestPath = resolve('.next', 'routes-manifest.json');
+
+  if (existsSync(routesManifestPath)) {
+    const routesManifest = JSON.parse(readFileSync(routesManifestPath, 'utf8')) as RoutesManifest;
+    const rewrites = routesManifest.rewrites.map((rewrite) => {
+      return {
+        source: rewrite.source,
+        destination: rewrite.destination,
+        locale: rewrite.locale,
+      };
+    });
+
+    // Save to the cache.
+    rewritesCache = rewrites;
+    return rewritesCache;
+  }
+
+  // If the server routes-manifest is not available, then get can get the rewrites from the client build manifest.
   const buildManifestPath = resolve('.next', 'build-manifest.json');
   const buildManifestContent = readFileSync(buildManifestPath, 'utf8');
 
@@ -35,6 +58,5 @@ export function getRewrites(): Rewrite[] {
 
   // Save to the cache.
   rewritesCache = clientBuildManifest.__BUILD_MANIFEST.__rewrites.afterFiles;
-
   return rewritesCache;
 }
