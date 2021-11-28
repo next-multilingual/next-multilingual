8a8cb075cf3427a9b8a6631ebbd8eb41472b3c68
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2021-11-27 21:39:58 -0500

    🐛 fix NEXT_PUBLIC_ORIGIN trailing slash bug

--- src/helpers/get-origin.ts
+++ src/helpers/get-origin.ts
@@ -1,35 +1,42 @@
+/** This is the "sanitized" origin to avoid re-validating each time `getOrigin` is called. */
+let origin;
+
 /**
  * Get the current environment's URL origin (protocol + domain).
  *
  * @returns The normalized (lowercase, without a trailing slash) URL origin used to generate fully-qualified URLs.
  */
 export function getOrigin(): string {
-  const origin = process.env.NEXT_PUBLIC_ORIGIN;
+  if (origin !== undefined) return origin;
 
-  if (!origin) {
+  const nextPublicOrigin = process.env.NEXT_PUBLIC_ORIGIN;
+
+  if (!nextPublicOrigin) {
     throw new Error(
       'Please add NEXT_PUBLIC_ORIGIN in your current environment variable with a fully-qualified URL (protocol + domain)'
     );
   }
 
-  try {
-    // Only valid URLs can create a URL object without throwing errors.
-    const originUrl = new URL(origin);
+  const matches = nextPublicOrigin.match(/^http[s]?:\/\/.+$/);
 
-    // Make sure that the configured URL has a valid protocol.
-    if (!['http', 'https'].includes(originUrl.protocol.slice(0, -1))) {
-      throw new Error();
-    }
-
-    // Make sure that the configured URL does not include a path (Next.js' `basePath` should be used instead).
-    if (originUrl.pathname !== '/') {
-      throw new Error();
-    }
-  } catch (error) {
+  if (!matches) {
     throw new Error(
-      'Please make sure that your NEXT_PUBLIC_ORIGIN environment variable is a fully-qualified URL (e.g. https://example.com) without any path'
+      'Please make sure that your NEXT_PUBLIC_ORIGIN includes either the `http` or `https` protocol and is a fully-qualified URL (e.g. https://example.com) without any path'
     );
   }
 
-  return (origin.endsWith('/') ? origin.slice(0, 1) : origin).toLowerCase();
+  const normalizedOrigin = nextPublicOrigin.endsWith('/')
+    ? nextPublicOrigin.slice(0, -1)
+    : nextPublicOrigin;
+
+  const slashCount = (normalizedOrigin.match(/\//g) || []).length;
+
+  if (slashCount > 2) {
+    throw new Error(
+      'Please remove any path from your NEXT_PUBLIC_ORIGIN fully-qualified URL (e.g. https://example.com)'
+    );
+  }
+
+  origin = nextPublicOrigin;
+  return origin;
 }
