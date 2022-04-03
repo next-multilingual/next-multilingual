f44a0a36ec855d73fa2063d107b5121852fb9922
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2022-04-02 22:24:43 -0400

    build: update to Next.js v12.1.4

--- src/Global.d.ts
+++
@@ -1,7 +0,0 @@
-// Workaround related to: https://github.com/vercel/next.js/issues/29788
-declare type StaticImageData = {
-  src: string;
-  height: number;
-  width: number;
-  placeholder?: string;
-};
