82e49d4f8dbd6a88e0c38a63aefb7889c28e4980
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2022-05-01 16:45:42 -0400

    build: update dependencies

--- example/pages/tests/dynamic-routes/[id].tsx
+++ example/pages/tests/dynamic-routes/[id].tsx
@@ -34,13 +34,12 @@ const Id: NextPage = () => {
             <td>{messages.format('rowNonLocalizedPagePath')}</td>
             <td>{pathname}</td>
           </tr>
-          {/* @see https://github.com/facebook/react/issues/24270 (re-enable test once the fix is available) */}
-          {/* <tr>
-            <td>{messages.format('rowLocalizedWithAsPath')}</td> */}
-          {/* Adding `suppressHydrationWarning` until
+          <tr>
+            <td>{messages.format('rowLocalizedWithAsPath')}</td>
+            {/* Adding `suppressHydrationWarning` until
             https://github.com/vercel/next.js/issues/32772 is resolved */}
-          {/* <td suppressHydrationWarning={true}>{asPath}</td>
-          </tr> */}
+            <td suppressHydrationWarning={true}>{asPath}</td>
+          </tr>
           <tr>
             <td>{messages.format('rowLocalizedWithUseLocalizedUrl')}</td>
             <td>{localizedUrl}</td>
